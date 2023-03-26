dd177a1ba579ceb3ff948b9b360bfba9f20ab605
mortezaahmadiaf <76720070+mortezaahmadiaf@users.noreply.github.com>
2023-03-26 13:19:38 +0430

    test

---
+++ build/js/3.bundle.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[3],{3:(s,e,t)=>{t.r(e),t.d(e,{default:()=>c});const c=Object.assign({},{test:"تست"})}}]);
+//# sourceMappingURL=3.bundle.js.map
