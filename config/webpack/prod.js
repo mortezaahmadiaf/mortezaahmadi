66027c7a63ba4c3806911c6f9ee2f02ea26c3595
mortezaahmadiaf <76720070+mortezaahmadiaf@users.noreply.github.com>
2023-03-26 13:47:58 +0430

    change

--- config/webpack/prod.js
+++ config/webpack/prod.js
@@ -10,7 +10,7 @@ module.exports = merge(commonConfig, {
   mode: "production",
   output: {
     filename: "js/bundle.js",
-    path: resolve(__dirname, "../../build"),
+    path: resolve(__dirname, "../../"),
     publicPath: "./",
   },
   module: {
@@ -26,7 +26,7 @@ module.exports = merge(commonConfig, {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: resolve(__dirname, "build/css"),
+              publicPath: resolve(__dirname, "/css"),
             },
           },
           "css-loader",
