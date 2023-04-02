4b75d84f1a7b94587c6a3ec6701eff6b3e90f843
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-02 09:16:50 +0430

    change

--- docs/js/947.bundle.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([[947],{947:(e,s,t)=>{t.r(s),t.d(s,{default:()=>a});const a=Object.assign({},{test:"تست"})}}]);
-//# sourceMappingURL=947.bundle.js.map
