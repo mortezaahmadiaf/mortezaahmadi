2f1eafae62f61e0d23148f67da166b3ad20a7e7d
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-02 09:11:07 +0430

    change

--- docs/js/3.bundle.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[3],{3:(s,e,t)=>{t.r(e),t.d(e,{default:()=>c});const c=Object.assign({},{test:"تست"})}}]);
-//# sourceMappingURL=3.bundle.js.map
