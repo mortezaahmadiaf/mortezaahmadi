2be1e0ccf8e2ffe343eb5ed39c27200d7176b902
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-02 09:00:55 +0430

    change

---
+++ config/webpack/common.js
@@ -0,0 +1,42 @@
+// shared config (dev and prod)
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { resolve } = require("path");
+
+module.exports = {
+  context: resolve(__dirname, "../../src"),
+  entry: "./index.tsx",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      config: path.resolve(__dirname, "../../config"),
+      app: path.resolve(__dirname, "../../src/"),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: [/\.(js|jsx)?$/],
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Morteza Ahmadi",
+      template: path.join(__dirname, "../../public", "index.html"),
+      base: ".",
+      inject: "head",
+      chunksSortMode: "auto",
+      attributes: {
+        // crossorigin: "anonymous",
+      },
+    }),
+  ],
+};
