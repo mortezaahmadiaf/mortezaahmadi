d7a53df9f577b6dde789df588ad146951cd0c0dd
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-05 17:39:04 +0430

    add flip card

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export * from "./translation";
+export * from "./flip-card/flip-card";
+export * from "./col";
+export * from "./row";
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./home.scss";
 import { Constantce } from "app/feature/utils";
-import { Translate, translate } from "app/components";
+import {
+  Translate,
+  translate,
+  FlipCardFull,
+  Row,
+  Col,
+  FlipCard,
+  FlipCardBack,
+  FlipCardFront,
+} from "app/components";
 import { useAppDispatch } from "app/feature/redux/hooks";
 import { changelang } from "app/feature/redux/slice/language";
 const Home = () => {
@@ -23,24 +32,38 @@ const Home = () => {
         </button>
       </section>
       <section className=" item-center m-0 p-0">
-        <div className=" flex-column card bg-primary col-3 p-4 m-1  item-center ">
-          <img
-            style={{ width: 160, height: 220 }}
-            src={require(`app/assets/images/${Constantce.P_Info.ima}`)}
-          />
-          <Translate
-            id="personalInfo.firstName"
-            param={{ firstName: Constantce.P_Info.F_Name }}
-          />
-          <Translate
-            id="personalInfo.lastName"
-            param={{ lastName: Constantce.P_Info.L_Name }}
-          />
+        <Col col={12}>
+          <Row>
+            <FlipCard>
+              <FlipCardFront>
+                <div className="bg-primary   m-0 p-0 ">
+                  <img
+                    className="bg-primary"
+                    style={{ width: 160, height: 220 }}
+                    src={require(`app/assets/images/${Constantce.P_Info.ima}`)}
+                  />
+                </div>
+              </FlipCardFront>
+              <FlipCardBack>
+                {" "}
+                <div className="flex-column bg-primary item-center text-dark  p-0 m-0">
+                  <Translate
+                    id="personalInfo.firstName"
+                    param={{ firstName: Constantce.P_Info.F_Name }}
+                  />
+                  <Translate
+                    id="personalInfo.lastName"
+                    param={{ lastName: Constantce.P_Info.L_Name }}
+                  />
 
-          <p className="mt-1">
-            <small>{Constantce.P_Info.Bio}</small>{" "}
-          </p>
-        </div>
+                  <p className="mt-1">
+                    <small>{Constantce.P_Info.Bio}</small>{" "}
+                  </p>
+                </div>
+              </FlipCardBack>
+            </FlipCard>
+          </Row>
+        </Col>
       </section>
     </div>
   );
