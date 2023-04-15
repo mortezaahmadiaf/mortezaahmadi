c712e31cf6c2765dba55613df091d816a9b4471e
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-15 09:26:46 +0430

    start working

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -5,29 +5,27 @@ import { Translate, translate } from "app/components";
 import { useAppDispatch } from "app/feature/redux/hooks";
 import { changelang } from "app/feature/redux/slice/language";
 const Home = () => {
-  const distpatch = useAppDispatch();
+  // const distpatch = useAppDispatch();
   return (
     <div className="d-flex w-100 h-100 text-white  flex-column m-0 p-0">
-      <section className="banner col-12   item-center ">
-        <button
-          onClick={() => distpatch(changelang("fa"))}
-          className="btn btn-primary text white mx-1"
-        >
-          fa
-        </button>
-        <button
-          onClick={() => distpatch(changelang("en"))}
-          className="btn btn-secoundry text white mx-1"
-        >
-          en
-        </button>
-      </section>
-      <section className=" item-center m-0 p-0">
-        <div className=" flex-column card bg-primary col-3 p-4 m-1  item-center ">
+      <section className="banner col-12 row m-0 d-flex justify-content-start align-items-center ">
+        <div className="  col-12 col-md-6" style={{ height: "auto" }}>
           <img
-            style={{ width: 160, height: 220 }}
+            className="col-3 col-md-5 rounded"
             src={require(`app/assets/images/${Constantce.P_Info.ima}`)}
           />
+        </div>
+        <div className="d-flex justify-content-start flex-column col-12 col-md-6">
+          <h1>
+            {" "}
+            <strong>MORTEZA AHMADI</strong>{" "}
+          </h1>
+          <h2>Full stack Developer(MERN ) with front end focus</h2>
+          <small>{Constantce.P_Info.Bio}</small>
+        </div>
+      </section>
+      <section className=" item-center m-0 p-0">
+        {/* <div className=" flex-column card bg-primary col-3 p-4 m-1  item-center ">
           <Translate
             id="personalInfo.firstName"
             param={{ firstName: Constantce.P_Info.F_Name }}
@@ -40,10 +38,24 @@ const Home = () => {
           <p className="mt-1">
             <small>{Constantce.P_Info.Bio}</small>{" "}
           </p>
-        </div>
+        </div> */}
       </section>
     </div>
   );
 };
 
 export default Home;
+{
+  /* <button
+onClick={() => distpatch(changelang("fa"))}
+className="btn btn-primary text white mx-1"
+>
+fa
+</button>
+<button
+onClick={() => distpatch(changelang("en"))}
+className="btn btn-secoundry text white mx-1"
+>
+en
+</button> */
+}
